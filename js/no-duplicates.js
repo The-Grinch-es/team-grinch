5044d2c6db3980103f53ee9a03f3d08d65e603ab
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2019-12-11 14:33:04 -0600

    added no-duplicates and links to problems in edabit

---
+++ js/no-duplicates.js
@@ -0,0 +1,12 @@
+function sortArray(arr){
+    var newArr = (""+arr).split(',');
+    console.log(newArr);
+    for (var i = 0; i <= newArr.length; i++){
+        if (newArr[i] === newArr.indexOf[i])
+            console.log(i);
+        // return i;
+
+    }
+
+}
+console.log(sortArray([1, 3, 3, 5, 5, 5]));
