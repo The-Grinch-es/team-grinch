0206e3e7026cdbbb63076c753d8b4866e7a5463e
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-11 21:58:12 -0600

    battleship still not  working

--- js/battleshipHFJS.js
+++ js/battleshipHFJS.js
@@ -1,4 +1,5 @@
 // Ship locations
+"use strict"
 var location1 = 3;
 var location2 = 4;
 var location3 = 5;
@@ -8,21 +9,23 @@ var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
-
+guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+console.log(location3)
 while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-    if(guess<0 || guess){
+    if(guess < 0 || guess >6){
         alert("Please enter a vaild cell number!");
     }else {
         guesses = guesses + 1;
 
         if (guess == location1 || guess == location2 || guess == location3){
+            alert("HIT");
             hits = hits +1;
-
-            if (hits == 3){
+            if (hits === 3){
                 isSunk = true;
                 alert("You sunk my battleship");
             }
+        }else {
+            alert("MISS!");
         }
 
     }
