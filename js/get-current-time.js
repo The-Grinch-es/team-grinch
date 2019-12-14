ab66d2d126ccaacbe9af7a81c610d64dd660c561
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-14 10:29:15 -0600

    practice arrays js

--- js/get-current-time.js
+++ js/get-current-time.js
@@ -1,4 +1,8 @@
-"use script"
+"use strict"
+
+// Write a JavaScript program to display the current day and time in the following format.
+//     Today is : Tuesday.
+//     Current time is : 10 PM : 30 : 38
 
 var today = new Date();
     var day = today.getDay();
