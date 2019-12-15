d7c89c21c091dc60eab35af7438a70a1b4d508b2
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2019-12-15 14:29:00 -0600

    adjusted format max-min

--- js/max-min.js
+++ js/max-min.js
@@ -4,10 +4,10 @@ function diffMaxMin(nums) {
 
 // console.log(diffMaxMin());
 
-console.log(diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]))// ➞ 82
+console.log(diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]));// ➞ 82
 // Smallest number is -50, biggest is 32.
 
-console.log(diffMaxMin([44, 32, 86, 19]))// ➞ 67
+console.log(diffMaxMin([44, 32, 86, 19]));// ➞ 67
 // Smallest number is 19, biggest is 86.
 
 // math apply  vs ...arr
